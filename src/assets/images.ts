87eeecdbd54bf3a47f522163c2d685e35c5f26f9
Axi <axidreamdragon@gmail.com>
2025-03-11 17:53:12 +0100

    Add extra default images

--- src/assets/images.ts
+++ src/assets/images.ts
@@ -1,9 +1,15 @@
 import texel from './texel.jpg';
 import vlieland from './vlieland.jpg';
+import hoofdplaat from './hoofdplaat.jpg';
+import b9088 from './b9088.jpg';
+import svedjehamn from './svedjehamn.jpg';
 
 const images = [
 	texel,
-	vlieland
+	vlieland,
+	hoofdplaat,
+	b9088,
+	svedjehamn
 ];
 
 export default images;
