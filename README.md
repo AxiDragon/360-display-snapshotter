b42b9a1267e166a29dac4192b916bd7f65132c0c
Axi <axidreamdragon@gmail.com>
2025-03-11 12:26:36 +0100

    Create base project

---
+++ README.md
@@ -0,0 +1,3 @@
+# 360 Display
+
+A basic app to display 360 images. Built with React and Three.js.
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      360 Displayer
+    </div>
+  );
+}
+
+export default App;
