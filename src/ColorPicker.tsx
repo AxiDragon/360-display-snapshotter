6e949b938eb558c2508b040284e1ed12c1a8a3d1
Axi <axidreamdragon@gmail.com>
2025-05-13 16:12:43 +0200

    Account for pixel ratio

--- src/ColorPicker.tsx
+++ src/ColorPicker.tsx
@@ -23,23 +23,20 @@ function ColorPicker() {
 
 	const pickColor = async (x: number, y: number) => {
 		gl.render(scene, camera);
+		const encodedUrl = gl.domElement.toDataURL("image/png");
+		const blob = await fetch(encodedUrl).then((response) => response.blob());
+		const url = URL.createObjectURL(blob);
 		const img = new Image();
 		//TODO: only render specific pixel?
-		img.src = gl.domElement.toDataURL("image/png");
+		img.src = url;
 
 		//NOTE: If I make some prior input (like a button that prevents accidental rotating),
 		// I could render a static image first and then get the color when the user selects something. 
 		// It would look less laggy
 		// then again, I am planning on using motion controls with phone
 		await new Promise<void>((res, rej) => {
-			img.onload = () => {
-				res();
-			};
-
-			img.onerror = (err) => {
-				console.error("Failed to load image:", err);
-				rej(err);
-			};
+			img.onload = () => res();
+			img.onerror = (err) => rej(err);
 		});
 
 		const canvas = document.createElement("canvas");
@@ -50,12 +47,18 @@ function ColorPicker() {
 			return;
 		}
 
+		//window.devicePixelRatio can result in decimal numbers, which dont work properly
+		const dpr = img.width / window.innerWidth;
 		canvas.width = img.width;
 		canvas.height = img.height;
 
 		ctx.drawImage(img, 0, 0);
 
-		const pixel = ctx.getImageData(x, y, 1, 1).data;
+		//image becomes double size for some reason - why?
+		console.log("Click position: ", x, y);
+		console.log("Image size: ", img.width, img.height);
+		console.log("Window size: ", window.innerWidth, window.innerHeight);
+		const pixel = ctx.getImageData(x * dpr, y * dpr, 1, 1).data;
 		const [r, g, b] = pixel;
 		const color = `rgb(${r}, ${g}, ${b})`;
 		console.log("Picked Color: %c   ", `background: ${color}; padding: 5px; border: 1px solid black`);
@@ -66,9 +69,15 @@ function ColorPicker() {
 			newColors.splice(maxColors);
 		}
 
-		console.log(newColors);
-
 		setColors(newColors);
+
+		//create a rectangle around the pixel for debugging
+		ctx.strokeStyle = "red";
+		ctx.lineWidth = 2;
+		ctx.strokeRect(x * dpr - 2, y * dpr - 2, 5, 5);
+
+		const canvasBlob = await fetch(canvas.toDataURL("image/png")).then((response) => response.blob());
+		// window.open(URL.createObjectURL(canvasBlob));
 	};
 
 	return (
